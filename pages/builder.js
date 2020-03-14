bd28c0609a06d8704d8fd78b42c448633c6bbdf8
Hunter Chang <hunter@madwiremedia.com>
2020-03-14 15:26:57 -0600

    initial commit

---
+++ pages/builder.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const BuilderIndex = () => <h1>Builder</h1>;
+
+export default BuilderIndex;
---
+++ pages/lessons.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const LessonsIndex = () => (
+  <div>
+    <h1>Lessons</h1>
+  </div>
+);
+
+export default LessonsIndex;
